fdaa58d8bf33f1570f36dee38e3553cf00b65699
Vincent Gim <d20.savras@gmail.com>
2018-10-16 21:55:55 -0700

    Initial commit for Chp18 Q1 array

---
+++ Chp18. Graphs/18.1.java
@@ -0,0 +1,41 @@
+class SearchMaze {
+    public static void main(String[] args) {
+      int[][] maze = new int[10][10];
+      // 1 == no path.
+      maze[0][0] = 1;   
+      maze[0]]6] = 1;
+      maze[0][7] = 1;
+      maze[1][2] = 1;
+      maze[2][0] = 1;
+      maze[2][2] = 1;
+      maze[2][5] = 1;
+      maze[2][6] = 1;
+      maze[2][8] = 1;
+      maze[2][9] = 1;
+      maze[3][3] = 1;
+      maze[3][4] = 1;
+      maze[3][5] = 1;
+      maze[3][8] = 1;
+      maze[4][1] = 1;
+      maze[4][2] = 1;
+      maze[5][1] = 1;
+      maze[5][2] = 1;
+      maze[5][2] = 1;
+      maze[5][5] = 1;
+      maze[5][7] = 1;
+      maze[5][8] = 1;
+      maze[6][4] = 1;
+      maze[7][0] = 1;
+      maze[7][2] = 1;
+      maze[7][4] = 1;
+      maze[7][6] = 1;
+      maze[8][0] = 1;
+      maze[8][2] = 1;
+      maze[8][3] = 1;
+      maze[8][7] = 1;
+      maze[8][8] = 1;
+      maze[8][9] = 1;
+      maze[9][8] = 1;
+      maze[9][9] = 1;
+    }
+}
