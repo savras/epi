3a44035560618b82a3a164fc43d87df7949925ee
Vincent Gim <d20.savras@gmail.com>
2018-10-20 23:15:16 -0700

    Completed solution

--- Chp18. Graphs/18.1.java
+++ Chp18. Graphs/18.1.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class SearchMaze {
+    static class Coordinate {
+      int row;
+      int col;
+    
+      public Coordinate() {}
+    
+      public Coordinate(int row, int col) {
+        this.row = row;
+        this.col = col;
+      }
+    }
+
+    static enum Color { WHITE, BLACK }
+
     final static int[][] DIRECTIONS = {{0,1},{1,0},{-1,0},{0,-1}};
+    static List<Coordinate> path = new ArrayList<Coordinate>();
 
     public static void main(String[] args) {
       int[][] maze = new int[10][10];
-
+      
       //col    0 1 2 3 4 5 6 7 8 9
       //r0     1 0 0 0 0 0 1 1 0 2
       //r1     0 0 1 0 0 0 0 0 0 0
@@ -17,9 +35,8 @@ class SearchMaze {
       //r9     0 0 0 0 0 0 0 0 1 1
       // 1 == blocked path.
       maze[0][0] = 1;   
-      maze[0][6] = 1;
+      maze[0][6] = 1; 
       maze[0][7] = 1;
-      maze[0][9] = 2;   // Destination
       maze[1][2] = 1;
       maze[2][0] = 1;
       maze[2][2] = 1;
@@ -52,28 +69,36 @@ class SearchMaze {
       maze[9][8] = 1;
       maze[9][9] = 1;
 
-      var hasPath = searchMaze(maze);
+      var hasPath = searchMaze(maze, path);
       System.out.println(hasPath);
     }
 
-    public static boolean searchMaze(int[][] maze){
-      return navigate(maze, 9, 0, 9, 0); // starts at x = 9 and y = 0
+    public static boolean searchMaze(int[][] maze, List<Coordinate> path){
+      var start = new Coordinate(9, 0);
+      var end = new Coordinate(0, 9);
+      return navigate(maze, start, end);
     }
 
-    public static boolean navigate(int[][] maze, int x, int y, int px, int py) {
-      if(!isXAndYWithinBounds(x, y)) { return false; }
-      if(maze[x][y] == 1) { return false; }   // Hit a wall
-      if(maze[x][y] > 1) { return true; }
-    
+    public static boolean navigate(int[][] maze, Coordinate cur, Coordinate dest) {
+      if(!isCoordinateWithinBoundsAndNotVisited(maze, cur)) { return false; }
+      if(cur.row == dest.row && cur.col == dest.col) { return true; }
+      
       for(int[] d : DIRECTIONS) {
-        if(px == x + d[0] && py == y + d[1]) { continue; }  // Don't go back to where we came from.
-          navigate(maze, x + d[0], y + d[1], x, y);
-      }
+        maze[cur.row][cur.col] = Color.BLACK.ordinal();
 
+        if(!navigate(maze, new Coordinate(cur.row + d[0], cur.col + d[1]), dest)){
+          maze[cur.row][cur.col] = Color.WHITE.ordinal();
+        }
+        else {
+          return true;
+        }
+      }
       return false;
     }
 
-    public static boolean isXAndYWithinBounds(int x, int y) {
-        return x >= 0 && x < 10 && y >= 0 && y < 10;
+    public static boolean isCoordinateWithinBoundsAndNotVisited(int[][] maze, Coordinate cur) {
+        return cur.row >= 0 && cur.row < 10 && 
+        cur.col >= 0 && cur.col < 10 &&
+        maze[cur.row][cur.col] == Color.WHITE.ordinal();
     }
 }
